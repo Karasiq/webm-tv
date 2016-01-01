ed7845d4ff0860909c195fd850f8e08c60942dfc
Le Karasique <karasiq.gh.cru5k@simplelogin.co>
2016-01-01 05:28:53 +0300

    working version

--- frontend/src/main/scala/com/karasiq/webmtv/frontend/app/WebmTvFrontend.scala
+++ frontend/src/main/scala/com/karasiq/webmtv/frontend/app/WebmTvFrontend.scala
@@ -1,23 +1,41 @@
 package com.karasiq.webmtv.frontend.app
 
+import org.scalajs.dom.ext.Ajax
 import org.scalajs.jquery.jQuery
 
 import scala.concurrent.Future
+import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
 
 object WebmTvFrontend extends JSApp {
-  def nextVideo(): Future[String] = {
-    // TODO: Ajax
-    Future.successful("https://2ch.hk/b/src/111694560/14515896032430.webm")
+  private def ajax(url: String): Future[String] = {
+    /* val promise = Promise[String]()
+    jQuery.ajax(js.Dynamic.literal(
+      url = url,
+      success = { (data: String, textStatus: String, jqXHR: JQueryXHR) ⇒
+        promise.success(data)
+      },
+      error = { (jqXHR: JQueryXHR, textStatus: String, errorThrow: String) ⇒
+        promise.failure(new Exception(errorThrow))
+      },
+      `type` = "GET"
+    ).asInstanceOf[JQueryAjaxSettings])
+    promise.future */
+    Ajax.get(url).map(_.responseText)
   }
+
+  def nextVideo(): Future[String] = ajax("/video")
 
   @JSExport
   override def main(): Unit = {
     jQuery { () ⇒
       val container = jQuery("#main-container")
-      container.append {
-        WebmTvHtml.videoContainer("https://2ch.hk/b/src/111694560/14515886592770.webm").render
+      nextVideo().onSuccess {
+        case url ⇒
+          container.append {
+            WebmTvHtml.videoContainer(url).render
+          }
       }
     }
   }
