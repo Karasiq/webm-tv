457ff8bf0620afba95189d0a207b652ac5d06c2c
Le Karasique <karasiq.gh.cru5k@simplelogin.co>
2016-01-01 05:28:53 +0300

    working version

--- src/main/scala/com/karasiq/webmtv/sosach/WebmStream.scala
+++ src/main/scala/com/karasiq/webmtv/sosach/WebmStream.scala
@@ -1,15 +1,23 @@
 package com.karasiq.webmtv.sosach
 
+import com.karasiq.webmtv.sosach.api.Thread
+
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext}
 import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
 
 object WebmStream {
+  private def threadFilter(thread: Thread): Boolean = {
+    thread.opPost.files.exists(_.name.endsWith(".webm")) &&
+      "аним[е|у|э]".r.findFirstIn(thread.opPost.comment.toLowerCase).isEmpty &&
+      "фап[- ]тред".r.findFirstIn(thread.opPost.comment.toLowerCase).isEmpty
+  }
+
   def apply(board: String)(implicit ec: ExecutionContext): Iterator[String] = {
     val threads = Try(Await.result(SosachApi.board(board), 1 minutes)) match {
       case Success(pages) ⇒
-        for (page <- pages; thread <- page.threads if thread.posts.flatMap(_.files).exists(_.name.endsWith(".webm"))) yield {
+        for (page <- pages; thread <- page.threads if threadFilter(thread)) yield {
           SosachApi.thread(board, thread.id)
         }
 
