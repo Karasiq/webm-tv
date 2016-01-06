ba434aada0110b39691d517f470fb1123521a542
Le Karasique <karasiq.gh.cru5k@simplelogin.co>
2016-01-06 17:12:45 +0300

    reshuffle button

---
+++ frontend/src/main/scala/com/karasiq/webmtv/frontend/app/Bootstrap.scala
@@ -0,0 +1,40 @@
+package com.karasiq.webmtv.frontend.app
+
+import org.scalajs.dom
+import rx._
+
+import scala.scalajs.js.ThisFunction
+import scalatags.JsDom.all
+import scalatags.JsDom.all._
+
+object Bootstrap {
+  def button: Tag = {
+    val margin = 5.px
+    all.button(`type` := "button", `class` := "btn btn-default btn-lg", marginLeft := margin, marginRight := margin)
+  }
+
+  def toggleButton(state: Var[Boolean]): Tag = {
+    this.button(onclick := ThisFunction.fromFunction1 { (btn: dom.Element) ⇒
+      if (btn.classList.contains("active")) {
+        btn.classList.remove("active")
+        state.update(false)
+      } else {
+        btn.classList.add("active")
+        state.update(true)
+      }
+    })
+  }
+
+  def glyphicon(name: String): Tag = {
+    span(`class` := s"glyphicon glyphicon-$name")
+  }
+
+  def row: Tag = {
+    div(`class` := "row")
+  }
+
+  def col(size: Int): Tag = {
+    assert(size >= 1 && size <= 12, "Invalid size")
+    div(`class` := s"col-md-$size")
+  }
+}
